2c96218fe81ae72ed9fcc80be08ca9e8bff4f559
peeoen <theerawat.pon@gmail.com>
2019-04-09 03:03:02 +0700

    Add variadic functions

---
+++ p12_variadic_functions/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Part 12: Variadic Functions")
+
+	// Variadic Functions เป็น function ที่สามารถระบุ parameters ได้ไม่รู้จบ
+
+	find(89, 89, 90, 95)
+    find(45, 56, 67, 45, 90, 109)
+    find(78, 38, 56, 98)
+	find(87)
+	
+
+	nums := []int{89, 90, 95}
+    find(89, nums...)
+}
+
+
+func find(num int, nums ...int) {  
+    fmt.Printf("type of nums is %T\n", nums)
+    found := false
+    for i, v := range nums {
+        if v == num {
+            fmt.Println(num, "found at index", i, "in", nums)
+            found = true
+        }
+    }
+    if !found {
+        fmt.Println(num, "not found in ", nums)
+    }
+    fmt.Printf("\n")
+}
